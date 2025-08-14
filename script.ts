db882e3f885018812ce461f1d6bbc4b6162c1f3f
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-08-14 12:31:43 -0300

    tipos primitivos

--- script.ts
+++ script.ts
@@ -1,5 +1,19 @@
-let username : string //neste caso "username só poderá receber valor como string. o comportamento muda se eu trocar para number ou boolean"
+//TIPOS PRIMITIVOS TYPESCRIPT
+
+
+//String para texto
+let username : string //neste caso "username" só poderá receber valor como string.
 
 username = "jonas"
-username = 27
-username = true
+//username = 27
+//username = true
+
+//Number para número
+let total: number
+total = 3
+total = 7.5
+
+//Boolean para verdadeiro ou falso.
+let isLoading : boolean
+isLoading = true
+isLoading = false
