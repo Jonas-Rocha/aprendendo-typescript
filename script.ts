1d2aa81d6853d5864701bd64774485475529b91e
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-08-14 12:24:31 -0300

    Iniciando estudos de typescript

---
+++ script.ts
@@ -0,0 +1,5 @@
+let username : string //neste caso "username só poderá receber valor como string. o comportamento muda se eu trocar para number ou boolean"
+
+username = "jonas"
+username = 27
+username = true
